b493651e5051350df2637063563dfe520fbc46a6
Eric Spencer <119877507+EricSpencer00@users.noreply.github.com>
2024-04-17 13:18:14 -0500

    fix money bugs, remove split option

--- BlackJack/main.py
+++ BlackJack/main.py
@@ -1,17 +1,18 @@
 import random
 
-print("Welcome to the text-based BlackJack game!")
+print("Welcome to the Command Line Blackjack!")
 
-money: int = 5000
+# Default value
+money = 5000
 ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 def pick_random_card():
     return random.choice(ranks)
 
-def calculate_hand_value(hand):
+def calculate_hand_value(cards):
     value = 0
     num_aces = 0
-    for card in hand:
+    for card in cards:
         if card in ["J", "Q", "K"]:
             value += 10
         elif card == "A":
@@ -26,16 +27,12 @@ def calculate_hand_value(hand):
             value += 1
     return value
 
-
-while money > 0:
-    print("You have $" + str(money) + " to play.")
-    user_input = int(input("How much do you want to bet?"))
-    if money >= user_input:
-        bet = user_input
-        money -= bet
-    else:
-        bet = money
-        money = 0
+def play_round(money):
+    bet = int(input("How much do you want to bet? "))
+    print(" * * * * ")
+    if bet > money:
+        print("You don't have enough money!")
+        return money
 
     user_cards = [pick_random_card(), pick_random_card()]
     dealer_cards = [pick_random_card(), pick_random_card()]
@@ -43,41 +40,41 @@ while money > 0:
     print("The Dealer has dealt you a " + user_cards[0] + 
           " and a " + user_cards[1] + 
           " and the Dealer is showing a " + dealer_cards[1])
-    
-    if user_cards[0] == user_cards[1]:
-        split_option = input("You have two of the same card, want to split? (y/n)")
-        if split_option.lower() == "y":
-            split_hand = [user_cards[0], pick_random_card()]
-            user_cards = [split_hand, [user_cards[1], pick_random_card()]]
-            print("You have split")
-        else:
-            print("You have not split")
-    
-    for hand in user_cards:
-        while True:
-            print("Your hand:", hand)
-            hand_value = calculate_hand_value(hand)
-            print("Your hand value:", hand_value)
-            if hand_value == 21:
-                winning: int = 1.5 * bet
-                money += winning
-                print("Blackjack! you win $" + str(winning))
-                break
-            elif hand_value > 21:
-                print("Bust! Loser")
-                break
 
-            action = input("Do you want to hit or stand?")
-            if action.lower() == "h":
-                hand.append(pick_random_card())
-                print("You drew:", hand[-1])
-            elif action.lower() == "s":
-                break
+    user_blackjack: bool = False
+    while True:
+        hand_value = calculate_hand_value(user_cards)
+        print("Your hand:", " ".join(user_cards) + ", Your hand value:", hand_value)
+        if hand_value == 21:
+            winning = 1.5 * bet
+            money += winning
+            print("Blackjack! You win $" + str(winning))
+            user_blackjack = True
+            break
+        elif hand_value > 21:
+            print("Bust! Loser")
+            money -= bet
+            break
+
+        action = input("Do you want to hit or stand? (h/s) ")
+        if action.lower() == "h":
+            user_cards.append(pick_random_card())
+            print("You drew:", user_cards[-1])
+            print(" * * * * ")
+        elif action.lower() == "s":
+            print("Standing...")
+            print(" * * * * ")
+            break
+
+        print("Dealer was showing a " + dealer_cards[0] + " and has revealed a " + dealer_cards[1] + "...")
+        print("Dealer's hand value: " + str(calculate_hand_value(dealer_cards)))
+        print(" * * * * ")
     
-    if hand_value <= 21:
+    if (hand_value <= 21 and not user_blackjack):
         while calculate_hand_value(dealer_cards) < 17:
-            dealer_cards.append(pick_random_card())
-            print("Dealer drew:", dealer_cards[-1])
+            new_card = pick_random_card()
+            dealer_cards.append(new_card)
+            print("Dealer drew:", new_card)
 
         dealer_hand_value = calculate_hand_value(dealer_cards)
         print("Dealer's hand value:", dealer_hand_value)
@@ -87,17 +84,23 @@ while money > 0:
             money += 2 * bet
         elif dealer_hand_value > hand_value:
             print("Dealer wins!")
+            money -= bet
         elif dealer_hand_value < hand_value:
             print("You win!")
             money += 2 * bet
         else:
             print("It's a push!")
-            money = bet
 
-        if money <= 0:
-            print("You spent all your money!")
-            break
+    return money
 
-        play_again = input("Do you want to play again? (y/n): ")
-        if play_again.lower() != "y":
-            break
+while money > 0:
+    print("You have $" + str(money) + " to play.")
+    money = play_round(money)
+    if money <= 0:
+        print("You are out of money!")
+        break
+
+    print("You have $" + str(money))
+    play_again = input("Do you want to play again? (y/n): ")
+    if play_again.lower() != "y":
+        break
